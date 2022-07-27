3f08ea97fdde4780d6d0516444905b0858717cd1
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-07-27 20:36:58 +0900

    feat/addClearSign

--- src/components/TaskItem.vue
+++ src/components/TaskItem.vue
@@ -3,7 +3,8 @@
     <div class="cleared-border" :class="cleared">
       <div>
         <q-card-section class="q-py-none q-px-sm card-name">
-          <span class="text-h6">{{getCards.cardName}}</span>
+          <q-input borderless autogrow v-model="getCards.cardName" class="col-11 text-h6" placeholder="cardName" />
+          <!-- <span class="text-h6" @click="setDetails = true">{{getCards.cardName}}</span> -->
         </q-card-section>
         <q-card-section class="row justify-start items-center q-py-none q-px-sm card-name">
           <div class="col-2">
@@ -16,17 +17,56 @@
             </q-tooltip>
           </div>
           <div class="col-3">
-            <q-btn flat @click="getCards.archives = true" icon="archive" />
+            <q-btn flat @click="archiveCard" icon="archive" />
             <q-tooltip>
               Archive
             </q-tooltip>
           </div>
-          <div class="col-6">
-            <q-select v-model="getCards.priority" :options="options" label="priority" />
+          <div class="col-6 q-py-sm">
+            <q-select dense filled borderless v-model="getCards.priority" :options="options" label="priority" />
           </div>
         </q-card-section>
       </div>
     </div>
+    <!-- <q-dialog v-model="setDetails">
+      <q-layout view="hhh lpr fff" container class="bg-white">
+        <q-header class="bg-blue-grey sp-none">
+          <q-toolbar>
+            <q-toolbar-title>
+              <q-input dense outlined bg-color="grey-4" v-model="getCards.cardName" label="change card title"
+                class="full-width" style="font-size: 25px;" />
+            </q-toolbar-title>
+            <q-btn dense flat round icon="menu" @click="rightDrawerOpen = !rightDrawerOpen" />
+            <q-btn flat v-close-popup round dense icon="close" />
+          </q-toolbar>
+        </q-header>
+        <q-footer class="bg-blue-grey">
+          <q-toolbar class="tb-pc-none">
+            <q-toolbar-title>
+              <q-input dense outlined bg-color="grey-4" v-model="getCards.cardName" label="change card title"
+                class="full-width" style="font-size: 25px;" />
+            </q-toolbar-title>
+            <q-btn flat v-close-popup round dense icon="close" />
+          </q-toolbar>
+        </q-footer>
+
+        <q-page-container>
+          <q-page padding>
+            <p>
+              <q-expansion-item v-model="expanded" icon="perm_identity" label="Description">
+                <q-card>
+                  <q-card-section>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem, eius reprehenderit eos corrupti
+                    commodi magni quaerat ex numquam, dolorum officiis modi facere maiores architecto suscipit iste
+                    eveniet doloribus ullam aliquid.
+                  </q-card-section>
+                </q-card>
+              </q-expansion-item>
+            </p>
+          </q-page>
+        </q-page-container>
+      </q-layout>
+    </q-dialog> -->
   </q-card>
 </template>
 
@@ -42,10 +82,31 @@
       return {
         getCards: this.card,
         options: ["none", "high", "middle", "low"],
+        drawerR: false,
+        setDetails: false,
+        expanded: true,
       }
     },
 
     methods: {
+      archiveCard() {
+        let message = "";
+        if (!this.getCards.checked) {
+          message = "The task is uncompleted. Do you archive?"
+        } else {
+          message = "Do you archive the task?"
+        }
+        this.$q.dialog({
+          title: 'Alert',
+          message,
+          cancel: {
+            push: true,
+            color: 'negative'
+          }
+        }).onOk(() => {
+          this.getCards.archives = true;
+        });
+      },
     },
 
     computed: {
@@ -139,5 +200,17 @@
   .gnavi .cleared div::before,
   .gnavi .cleared div::after {
     height: 100%;
+  }
+
+  .sp-none {
+    @media screen and (max-width:599px) {
+      display: none;
+    }
+  }
+
+  .tb-pc-none {
+    @media screen and (min-width:600px) {
+      display: none;
+    }
   }
 </style>
