45e4b8a51e27a6aa96ce1ae177b14ae4e65d69f7
Hirotada Takeuchi <t.hir0tada.biz@gmail.com>
2022-07-27 13:27:07 +0900

    feat/archiveSectionCard

--- src/components/TaskColumn.vue
+++ src/components/TaskColumn.vue
@@ -1,15 +1,34 @@
 <template>
-  <div class="q-pa-sm w-300px">
+  <div class="q-pa-sm w-300px" v-if="!getSection.archives">
     <q-card class="my-card bg-blue-grey-1">
-      <q-card-section class="section-name order-xs-first">
-        <q-input borderless v-model="getSection.sectionName" class="text-h6" label="sectionName" />
+      <q-card-section class="row justify-around section-name order-xs-first">
+        <q-input borderless v-model="getSection.sectionName" class="text-h6" placeholder="sectionName" />
+        <q-btn flat padding="xs" icon="more_vert">
+          <q-menu transition-show="scale" transition-hide="scale">
+            <q-list style="min-width: 100px">
+              <q-item clickable @click="archiveSection">
+                <q-item-section>archive section</q-item-section>
+              </q-item>
+              <q-separator />
+              <q-item clickable @click="sortPriority">
+                <q-item-section>sort priority</q-item-section>
+              </q-item>
+            </q-list>
+          </q-menu>
+        </q-btn>
       </q-card-section>
       <q-card-section class="h-full q-py-none">
         <div v-for="card of getSection.cardList" :key="card.cardPosNum">
           <TaskItem :card="card"></TaskItem>
         </div>
+        <q-card class="my-card q-mb-md" v-if="newCardInput">
+          <q-card-section class="q-py-none q-px-sm card-name text-h6">
+            <q-input borderless class="new-card" v-model="newCard" placeholder="cardName" />
+            <q-btn label="add" color="primary" @click="addCard" class="q-my-sm" />
+          </q-card-section>
+        </q-card>
       </q-card-section>
-      <q-btn flat class="full-width q-py-sm bg-dark text-white" label="＋ add card" @click="addCard" />
+      <q-btn flat class="full-width q-py-sm bg-dark text-white" label="＋ add card" @click="addSectionInput" />
     </q-card>
   </div>
 </template>
@@ -31,38 +50,55 @@
     data() {
       return {
         getSection: this.section,
+        newCard: "",
+        newCardInput: false,
       }
     },
 
     methods: {
+      addSectionInput() {
+        this.newCardInput = !this.newCardInput;
+        this.$nextTick(() => {
+          if (document.querySelector(".new-card input") !== null) {
+            document.querySelector(".new-card input").focus();
+          }
+        });
+      },
       addCard() {
+        this.newCardInput = !this.newCardInput;
         const date = new Date();
         this.getSection.cardList.push({
-          "cardName": "",
-          "cardPosNum": this.getSection.cardList.length + 1,
+          "cardName": this.newCard ? this.newCard : "No card title",
+          // "cardPosNum": this.getSection.cardList.length + 1,
           "cardContent": "content",
-          "createDate": date.toLocaleString(),
-          "deadLine": "",
-          "checkList": {},
-          "cardTags": [],
+          // "createDate": date.toLocaleString(),
+          // "deadLine": "",
+          // "checkList": {},
+          // "cardTags": [],
+          "priority": "",
+          "checked": false,
           "archives": false,
-          "cardComment": "comment",
-          "cardStar": false
+          // "cardComment": "comment",
+        });
+        this.newCard = ""
+      },
+      archiveSection() {
+        this.$q.dialog({
+          title: 'Alert',
+          message: 'All cards in the section are also archived. Are you sure??',
+          cancel: true
+        }).onOk(() => {
+          this.getSection.archives = true;
+          for (let i = 0; i < this.getSection.cardList.length; i++) {
+            this.getSection.cardList[i].archives = true;
+          }
         });
       },
     },
-
     computed: {
     },
 
     mounted() {
-      // get out of focus
-      const sectionName = document.querySelector(".section-name input");
-      sectionName.addEventListener("keydown", (e) => {
-        if (e.key === "Enter") {
-          document.activeElement.blur();
-        }
-      });
     },
   }
 </script>
